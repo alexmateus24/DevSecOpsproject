e718664c60968660050d892656827d244ede5958
alexmateus24 <alexmateus247@live.com>
2026-01-11 12:20:56 +0000

    appcode

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformseptbucketsun24" {
+  bucket = "terraformseptbucketsun24"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformseptbucketsun24"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
